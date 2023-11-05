54821c3a763c31ba7f97038795d8c5b515214ab3
liuwei <roostinghawk@163.com>
2023-11-05 17:05:33 +0800

    update getup.py

--- get_up.py
+++ get_up.py
@@ -74,17 +74,17 @@ def make_get_up_message(bing_cookie):
     #     model="gpt-3.5-turbo",
     #     messages=ms,
     # )
-    sentence_en = (
-        completion["choices"][0].get("message").get("content").encode("utf8").decode()
-    )
+    # sentence_en = (
+    #     completion["choices"][0].get("message").get("content").encode("utf8").decode()
+    # )
     link = ""
     try:
-        link = make_pic_and_save(sentence_en, bing_cookie)
+        link = make_pic_and_save(sentence, bing_cookie)
     except Exception as e:
         print(str(e))
         # give it a second chance
         try:
-            link = make_pic_and_save(sentence_en, bing_cookie)
+            link = make_pic_and_save(sentence, bing_cookie)
         except Exception as e:
             print(str(e))
     body = GET_UP_MESSAGE_TEMPLATE.format(
